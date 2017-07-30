485b3d75d4631f4bcbc69a6af08c58d4aba53419
Chirag Mahapatra <chirag.mahapatra@gmail.com>
2017-07-30 02:03:09 -0700

    fixed handler

--- messenger_bot/postback_handler.py
+++ messenger_bot/postback_handler.py
@@ -28,20 +28,6 @@ def handle(event):
         payload = ast.literal_eval(payload)
         if 'test' in payload and payload['test'] is True:
             handle_test(payload, sender_id)
-            if 'result' in payload:
-                result = payload['payload']
-            else:
-                result = []
-            if payload['id'] == payload['correct']:
-                result.append({'qid': payload['qid'], 'correct': True})
-            else:
-                result.append({'qid': payload['qid'], 'correct': False})
-            topics = payload['topics']
-            question = question_from_topic(topics.pop())
-            options = options_and_answer(question[ID])
-            send_question(sender_id, question, options,
-                          remaining=payload['remaining'] - 1,
-                          topics=topics, diagnostic=True, test=True)
         else:
             handle_question(payload, sender_id)
 
