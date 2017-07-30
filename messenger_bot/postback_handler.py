ced642ace66ada1b42c738a4428560ce5bf12c46
Chirag Mahapatra <chirag.mahapatra@gmail.com>
2017-07-30 01:52:36 -0700

    added handling for diagnostics

--- messenger_bot/postback_handler.py
+++ messenger_bot/postback_handler.py
@@ -27,6 +27,7 @@ def handle(event):
         payload = event['postback']['payload']
         payload = ast.literal_eval(payload)
         if 'test' in payload and payload['test'] is True:
+            handle_test(payload, sender_id)
             if 'result' in payload:
                 result = payload['payload']
             else:
@@ -42,9 +43,37 @@ def handle(event):
                           remaining=payload['remaining'] - 1,
                           topics=topics, diagnostic=True, test=True)
         else:
-            if payload['id'] == payload['correct']:
-                send_text_message(sender_id, "That's the right answer!")
-                send_image(sender_id, random.choice(correct_gifs))
-            else:
-                send_text_message(sender_id, "Sorry! That's not correct.")
-                send_image(sender_id, random.choice(wrong_gifs))
+            handle_question(payload, sender_id)
+
+
+def handle_test(payload, sender_id):
+    if 'result' in payload:
+        result = payload['payload']
+    else:
+        result = []
+    if payload['id'] == payload['correct']:
+        result.append({'qid': payload['qid'], 'correct': True})
+    else:
+        result.append({'qid': payload['qid'], 'correct': False})
+    if remaining == 0:
+        count_correct = sum([1 if res['correct'] else 0 for res in result])
+        count_total = len(result)
+        send_text_message(sender_id, "You got {0} out of {1}".format(
+            count_correct, count_total
+        ))
+    else:
+        topics = payload['topics']
+        question = question_from_topic(topics.pop())
+        options = options_and_answer(question[ID])
+        send_question(sender_id, question, options,
+                      remaining=payload['remaining'] - 1,
+                      topics=topics, diagnostic=True, test=True, result=result)
+
+
+def handle_question(payload, sender_id):
+    if payload['id'] == payload['correct']:
+        send_text_message(sender_id, "That's the right answer!")
+        send_image(sender_id, random.choice(correct_gifs))
+    else:
+        send_text_message(sender_id, "Sorry! That's not correct.")
+        send_image(sender_id, random.choice(wrong_gifs))
