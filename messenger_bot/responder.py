abc988ebeee6d9001aefd38681d6632345749cb8
Chirag Mahapatra <chirag.mahapatra@gmail.com>
2017-07-29 20:55:29 -0700

    adding onboarding message

--- messenger_bot/responder.py
+++ messenger_bot/responder.py
@@ -25,6 +25,13 @@ def response(message_text, sender_id):
 def greeting_flow(sender_id, response):
     send_text_message(sender_id, response[RESULT][FULFILLMENT][SPEECH])
     send_happy_gif(sender_id)
+    send_text_message(sender_id, 'My name is Noah, and I will be your '
+                      'SAT Buddy to help you get your desired score!')
+    send_text_message(sender_id, 'To begin with, we would do a quick '
+                      'assessment of your SAT concepts by asking you '
+                      '4 questions at a time and then coach you on your '
+                      'weak areas. :)')
+    send_text_message(sender_id, 'Let me know when you are ready to start.')
 
 
 def send_happy_gif(sender_id):
