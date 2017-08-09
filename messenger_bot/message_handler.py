d8464eb56bfc898baed6dc7d4622069a2ccddb20
Chirag Mahapatra <chirag.mahapatra@gmail.com>
2017-08-08 22:20:57 -0700

    questions answered feature

--- messenger_bot/message_handler.py
+++ messenger_bot/message_handler.py
@@ -1,5 +1,7 @@
 from messenger_bot.consts import *
 from database.db_api import question_from_topic, options_and_answer
+from database.diagnostic import questions_answered_today, \
+    questions_answered_last_week, questions_answered_last_month
 from messenger_bot.api_ai import APIAI
 from messenger_bot.logger import log
 from messenger_bot.sender import send_text_message, send_question, \
@@ -35,7 +37,18 @@ def handle_message(message_text, sender_id, request_id):
 
 
 def questions_answered_flow(sender_id, response):
-    send_text_message(sender_id, response[RESULT][FULFILLMENT][SPEECH])
+    time_periods = response[RESULT][PARAMETERS][TIME_PERIODS]
+    if time_periods == '' or time_periods.lower() == 'today':
+        time_periods = 'today'
+        questions_answered = questions_answered_today(sender_id)
+    elif time_periods.lower() == 'last month':
+        time_periods = 'last month'
+        questions_answered = questions_answered_last_month(sender_id)
+    elif time_periods.lower() == 'last week':
+        time_periods = 'last week'
+        questions_answered = questions_answered_last_week(sender_id)
+    send_text_message(sender_id, 'You have answered {0} questions {1}'.format(
+        questions_answered, time_periods))
 
 
 def test_start_flow(sender_id, response):
